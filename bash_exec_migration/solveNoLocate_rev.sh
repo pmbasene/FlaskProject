1b7db9f30cd4959de10f6bf80c849450908c2575
pmbasene <pmbasene@gmail.com>
2020-05-22 05:05:37 +0200

    version 1 terminé bis

--- bash_exec_migration/solveNoLocate_rev.sh
+++ bash_exec_migration/solveNoLocate_rev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=run.py runsever
 export FLASK_ENV=development
-flask db revision --rev-id 2e7abe5675bf
+flask db revision --rev-id 8c8df735ce3c
 flask db migrate -m "init"
 flask db upgrade
--- bash_exec_migration/update.sh
+++ bash_exec_migration/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export FLASK_APP=run.py runsever
 export FLASK_ENV=development
-flask db migrate -m "update"
+flask db migrate -m $1
 flask db upgrade
